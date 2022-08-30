28354ce2cf67e189cbe21c20b1520c400f902167
Remix Workspace <>
2022-08-30 15:25:02 +0100

    funding method updates

---
+++ funding.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT 
+
+pragma solidity ^0.8.0;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract Funding{
+    mapping(address => uint256) public donorAddressFunds;
+    function fund() public payable{
+        donorAddressFunds[msg.sender] += msg.value;
+    }
+}
